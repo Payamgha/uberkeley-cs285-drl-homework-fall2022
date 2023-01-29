41436c807b0d6ef28873c1f559a443d2923fbc01
Payam Ghassemi <payamgha@buffalo.edu>
2023-01-28 17:58:36 -0800

    Complete train_agent()

--- hw1/cs285/infrastructure/rl_trainer.py
+++ hw1/cs285/infrastructure/rl_trainer.py
@@ -196,12 +196,13 @@ class RL_Trainer(object):
             # TODO sample some data from the data buffer
             # HINT1: use the agent's sample function
             # HINT2: how much data = self.params['train_batch_size']
-            ob_batch, ac_batch, re_batch, next_ob_batch, terminal_batch = TODO
+            ob_batch, ac_batch, re_batch, next_ob_batch, terminal_batch = self.agent.sample(
+                self.params["train_batch_size"])
 
             # TODO use the sampled data to train an agent
             # HINT: use the agent's train function
             # HINT: keep the agent's training log for debugging
-            train_log = TODO
+            train_log = self.agent.train(ob_batch, ac_batch, re_batch, next_ob_batch, terminal_batch)
             all_logs.append(train_log)
         return all_logs
 
